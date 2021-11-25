c9267c837ea270b8c876f3ab3583e495d232b830
aayushi1306 <sharmaayushi63411@gmail.com>
2021-11-25 12:05:33 -0800

    v1

---
+++ index.php
@@ -0,0 +1,30 @@
+<!-- start php tag -->
+<?php 
+
+    // Single line comment
+    #  Single line comment
+
+    $name = 'Aayushi';
+    
+    $address = 'Singoli';
+
+    echo $name;
+    echo '<br>';
+    
+    echo "<br>";
+    echo $address;
+
+?>
+<?php 
+
+
+?>
+<?php 
+
+
+?>
+<?php 
+
+
+?>
+ <!-- end php tag  -->
